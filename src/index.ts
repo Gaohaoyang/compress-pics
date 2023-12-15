275582b6d0e3d7227c037524cddd4a80ac3462a5
Haoyang Gao <gaohaoyang126@126.com>
2023-12-15 14:39:19 -0500

    feat: 🎸 update

---
+++ src/index.ts
@@ -0,0 +1,64 @@
+import { readFiles } from './readFiles'
+import promptSync from 'prompt-sync'
+import fs from 'fs'
+import path from 'path'
+import tinify from 'tinify'
+
+const prompt = promptSync()
+
+const init = async () => {
+  tinify.key = 'YC4RydXcYxwqH101nCY66L9LM0Yk53s7'
+
+  const sizeBefore = getFileSizeInBytes('examples/images/coupon/HongbaoVip.png')
+  console.log(sizeBefore)
+  tinify.fromFile('examples/images/coupon/HongbaoVip.png').toFile('examples/images/coupon/HongbaoVip.png')
+  const sizeAfter = getFileSizeInBytes('examples/images/coupon/HongbaoVip.png')
+  console.log(sizeAfter)
+  console.log('rate ', (sizeAfter - sizeBefore) / sizeBefore)
+
+  // const folderPath = prompt('images folder path:', './examples/images')
+  // let size = 0
+  // let count = 0
+  // walkSync(folderPath, (filePath: string, dirent) => {
+  //   if (
+  //     dirent.name.endsWith('.png') ||
+  //     dirent.name.endsWith('.jpg') ||
+  //     dirent.name.endsWith('.jpeg') ||
+  //     dirent.name.endsWith('.gif') ||
+  //     dirent.name.endsWith('.svg')
+  //   ) {
+  //     size += getFileSizeInBytes(filePath)
+  //     count++
+  //     console.log(filePath)
+  //     // const source = tinify.fromFile(filePath)
+
+  //     // source.toFile(filePath)
+  //   }
+  // })
+
+  // console.log('count', count)
+  // console.log('size', size)
+  // console.log('size', (size / 1024 / 1024).toFixed(2) + 'MB')
+}
+
+const getFileSizeInBytes = (filename: string) => {
+  var stats = fs.statSync(filename)
+  var fileSizeInBytes = stats.size
+  return fileSizeInBytes
+}
+
+const walkSync = (
+  currentDirPath: string,
+  callback: (filePath: string, dirent: fs.Dirent) => void
+) => {
+  fs.readdirSync(currentDirPath, { withFileTypes: true }).forEach((dirent) => {
+    const filePath = path.join(currentDirPath, dirent.name)
+    if (dirent.isFile()) {
+      callback(filePath, dirent)
+    } else if (dirent.isDirectory()) {
+      walkSync(filePath, callback)
+    }
+  })
+}
+
+init()
